64c1784699e063fabb3178b7abe84ef2c46916ea
programmerarpoolen <bill@programmerarpoolen.com>
2018-07-26 01:29:26 +0200

    Update scheduled.py
    Adding max on time for manual irrigation of 5 minutes, to ensure irrigation is not accidentally started and then forgotten

--- scheduled.py
+++ scheduled.py
@@ -5,7 +5,7 @@ from datetime import datetime, time
 import MySQLdb
 import RPi.GPIO as GPIO
 import time
-from functions import dbfetch,dbupdate,dolog,relay_manual,relay_delay,sysreboot
+from functions import dbfetch,dbupdate,dolog,relay_manual,relay_delay,sysreboot,mandiff
 import logging
         
 #Sleeping for 3 seconds
@@ -17,10 +17,18 @@ while True:
     #Getting settings table from DB
     irrigatedata = dbfetch('IRRIGATE_NOW','weather_settings')
     rebootdata = dbfetch('REBOOTNOW','weather_settings')
+    if irrigatedata == 3:
+        difference = mandiff()
+        if difference/60 > 5:
+            irrigatedata = 2
+        else:
+            print("Time doesn't exceed 5 minutes")
     
     if irrigatedata == 1:
         dbupdate('IRRIGATE_NOW','weather_settings','3')
         print("Irrigating now")
+        nowtime = time.strftime("%Y-%m-%d %H:%M:%S")
+        dbupdate('MANTIME','weather_settings',str(nowtime))
         relay_manual('on')
     
     if irrigatedata == 2:
